2b461732e2e5fd30e92b0024484a3804dad5acce
jonnathans06 <jhonasaav@gmail.com>
2025-05-24 14:03:32 -0500

    feature(UPS-015)creaciones de paneles sin implementar metodos

--- src/main/java/ec/edu/ups/poo/ventanasProducto/VentanaProducto.java
+++ src/main/java/ec/edu/ups/poo/ventanasProducto/VentanaProducto.java
@@ -21,5 +21,111 @@ public class VentanaProducto extends Frame {
 
     public VentanaProducto(Frame ventanaAnterior, List<Proveedor> proveedores, List<ProductoAlimento> productosAlimento,
                            List<ProductoTecnologia> productosTecnologia, List<Producto> productos) {
+        this.ventanaAnterior = ventanaAnterior;
+        this.proveedores = proveedores;
+        this.productosAlimento = productosAlimento;
+        this.productos = productos;
+
+        Color azul = new Color(87, 124, 178);// Asignar un color
+
+        setTitle("Productos");
+        setSize(1500, 800);
+        setLocationRelativeTo(null);
+        setLayout(new BorderLayout());
+        setResizable(false);
+
+        // Título
+        Label titulo = new Label("PRODUCTOS", Label.CENTER);
+        titulo.setFont(new Font("Arial", Font.BOLD, 64));
+        titulo.setForeground(Color.white);
+        add(titulo, BorderLayout.NORTH);
+        Panel panelSuperior = new Panel(new GridLayout(2,1));
+        panelSuperior.setPreferredSize(new Dimension(1500, 230));
+        panelSuperior.setBackground(azul);
+        panelSuperior.add(titulo);
+        Panel panelSuperio2 = new Panel(new FlowLayout(FlowLayout.CENTER));
+        panelSuperio2.setBackground(Color.white);
+        panelSuperior.add(panelSuperio2);
+        add(panelSuperior, BorderLayout.NORTH);
+
+        // Panel Central
+        Panel panelCentral = new Panel(new GridLayout(2, 1, 5, 0));
+
+        // Panel para la opción "Producto Alimentos"
+        Panel panelAlimentos = new Panel(new FlowLayout(FlowLayout.CENTER));
+
+        // Etiqueta Producto Alimentos
+        Label etiquetaAlimentos = new Label("Producto Alimentos");
+        etiquetaAlimentos.setFont(new Font("Arial", Font.PLAIN, 18));
+        etiquetaAlimentos.setPreferredSize(new Dimension(400, 30));
+
+        // Botón Producto Alimentos
+        Button botonAlimentos = new Button("Seleccionar");
+        botonAlimentos.setFont(new Font("Arial", Font.PLAIN, 14));
+
+        botonAlimentos.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        // Agregar etiqueta y botón al panel
+        panelAlimentos.add(etiquetaAlimentos);
+        panelAlimentos.add(botonAlimentos);
+        panelCentral.add(panelAlimentos);
+
+        // Panel para la opción "Producto Tecnología"
+        Panel panelTecnologia = new Panel(new FlowLayout(FlowLayout.CENTER, 20, 10));
+
+        // Etiqueta Producto Tecnología
+        Label etiquetaTecnologia = new Label("Producto Tecnología");
+        etiquetaTecnologia.setFont(new Font("Arial", Font.PLAIN, 18));
+        etiquetaTecnologia.setPreferredSize(new Dimension(400, 30));
+
+        // Botón Producto Tecnología
+        Button botonTecnologia = new Button("Seleccionar");
+        botonTecnologia.setFont(new Font("Arial", Font.PLAIN, 14));
+
+        botonTecnologia.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        // Agregar etiqueta y botón al panel
+        panelTecnologia.add(etiquetaTecnologia);
+        panelTecnologia.add(botonTecnologia);
+        panelCentral.add(panelTecnologia);
+
+        // Agregar el panel central
+        add(panelCentral, BorderLayout.CENTER);
+
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                ventanaAnterior.setVisible(true);
+                dispose();
+            }
+        });
+
+
+        // Panel inferior con botón "Volver"
+        Panel panelInferior = new Panel(new FlowLayout(FlowLayout.CENTER, 20, 10));
+        panelInferior.setBackground(Color.lightGray);
+        Button btnVolver = new Button("Volver");
+        btnVolver.setFont(new Font("Arial", Font.PLAIN, 18));
+        btnVolver.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                ventanaAnterior.setVisible(true);
+                dispose(); // Oculta la ventana actual
+            }
+        });
+        panelInferior.add(btnVolver);
+        add(panelInferior, BorderLayout.SOUTH);
+
+        // Mostrar la ventana
+        setVisible(true);
     }
 }
--- src/main/java/ec/edu/ups/poo/ventanasProducto/VentanaProductoTecnologia.java
+++ src/main/java/ec/edu/ups/poo/ventanasProducto/VentanaProductoTecnologia.java
@@ -23,5 +23,140 @@ public class VentanaProductoTecnologia extends Frame {
 
     public VentanaProductoTecnologia(Frame ventanaAnterior, List<Proveedor> proveedores, List<ProductoTecnologia> productosTecnologia,
                                      List<Producto> productos) {
+        this.ventanaAnterior = ventanaAnterior;
+        this.proveedores = proveedores;
+        this.productosTecnologia = productosTecnologia;
+        this.productos = productos;
+
+        setTitle("Productos Tecnología");
+        setSize(1500, 800);
+        setLocationRelativeTo(null);
+        setLayout(new BorderLayout());
+        setResizable(false);
+
+        Color azul = new Color(87, 124, 178);
+
+        //titulo
+        Label titulo = new Label("PRODUCTOS TECNOLOGÍA", Label.CENTER);
+        titulo.setFont(new Font("Arial", Font.BOLD, 50));
+        titulo.setForeground(Color.white);
+        Panel panelSuperior = new Panel(new GridLayout(2,1));
+        Panel panelSuperior2 = new Panel(new FlowLayout(FlowLayout.CENTER));
+        panelSuperior.setPreferredSize(new Dimension(1500, 230));
+        panelSuperior.setBackground(azul);
+        panelSuperior.add(titulo);
+        panelSuperior2.setBackground(Color.white);
+        panelSuperior.add(panelSuperior2);
+
+        Panel panelCentral = new Panel(new GridLayout(6, 1, 10, 10));
+
+        // ID
+        Panel panelID = new Panel(new FlowLayout(FlowLayout.CENTER));
+        Label etiquetaID = new Label("Ingrese el ID del producto:");
+        etiquetaID.setFont(new Font("Arial", Font.PLAIN, 18));
+        etiquetaID.setPreferredSize(new Dimension(400, 30));
+        textFieldID = new TextField();
+        textFieldID.setFont(new Font("Arial", Font.PLAIN, 18));
+        textFieldID.setPreferredSize(new Dimension(400, 30));
+        panelID.add(etiquetaID);
+        panelID.add(textFieldID);
+
+        // Nombre
+        Panel panelNombre = new Panel(new FlowLayout(FlowLayout.CENTER));
+        Label etiquetaNombre = new Label("Ingrese el nombre del producto:");
+        etiquetaNombre.setFont(new Font("Arial", Font.PLAIN, 18));
+        etiquetaNombre.setPreferredSize(new Dimension(400, 30));
+        textFieldNombre = new TextField();
+        textFieldNombre.setFont(new Font("Arial", Font.PLAIN, 18));
+        textFieldNombre.setPreferredSize(new Dimension(400, 30));
+        panelNombre.add(etiquetaNombre);
+        panelNombre.add(textFieldNombre);
+
+        // Precio
+        Panel panelPrecio = new Panel(new FlowLayout(FlowLayout.CENTER));
+        Label etiquetaPrecio = new Label("Ingrese el precio del producto:");
+        etiquetaPrecio.setFont(new Font("Arial", Font.PLAIN, 18));
+        etiquetaPrecio.setPreferredSize(new Dimension(400, 30));
+        textFieldPrecio = new TextField();
+        textFieldPrecio.setFont(new Font("Arial", Font.PLAIN, 18));
+        textFieldPrecio.setPreferredSize(new Dimension(400, 30));
+        panelPrecio.add(etiquetaPrecio);
+        panelPrecio.add(textFieldPrecio);
+
+        // Gama
+        Panel panelGama = new Panel(new FlowLayout(FlowLayout.CENTER));
+        Label etiquetaGama = new Label("Ingrese la gama del producto:");
+        etiquetaGama.setFont(new Font("Arial", Font.PLAIN, 18));
+        etiquetaGama.setPreferredSize(new Dimension(400, 30));
+        textFieldGama = new TextField();
+        textFieldGama.setFont(new Font("Arial", Font.PLAIN, 18));
+        textFieldGama.setPreferredSize(new Dimension(400, 30));
+        panelGama.add(etiquetaGama);
+        panelGama.add(textFieldGama);
+
+        // Proveedor ID
+        Panel panelProveedorID = new Panel(new FlowLayout(FlowLayout.CENTER));
+        Label etiquetaProveedorID = new Label("Ingrese el ID del proveedor:");
+        etiquetaProveedorID.setFont(new Font("Arial", Font.PLAIN, 18));
+        etiquetaProveedorID.setPreferredSize(new Dimension(400, 30));
+        textFieldProveedorID = new TextField();
+        textFieldProveedorID.setFont(new Font("Arial", Font.PLAIN, 18));
+        textFieldProveedorID.setPreferredSize(new Dimension(400, 30));
+        panelProveedorID.add(etiquetaProveedorID);
+        panelProveedorID.add(textFieldProveedorID);
+
+        // Inferior
+        Panel panelInferior = new Panel(new FlowLayout(FlowLayout.CENTER, 20, 10));
+        panelInferior.setBackground(Color.lightGray);
+        btnMostrarProveedores = new Button("Mostrar Proveedores");
+        btnMostrarProveedores.setFont(new Font("Arial", Font.PLAIN, 18));
+        btnAgregar = new Button("Agregar Producto");
+        btnAgregar.setFont(new Font("Arial", Font.PLAIN, 18));
+        btnVolver = new Button("Volver");
+        btnVolver.setFont(new Font("Arial", Font.PLAIN, 18));
+        Label etiquetaEstado = new Label("Estado de registro:");
+        etiquetaEstado.setFont(new Font("Arial", Font.PLAIN, 18));
+        textFieldEstado = new TextField(" En proceso ... ", 20);
+        textFieldEstado.setEditable(false);
+        panelInferior.add(btnMostrarProveedores);
+        panelInferior.add(btnAgregar);
+        panelInferior.add(btnVolver);
+        panelInferior.add(etiquetaEstado);
+        panelInferior.add(textFieldEstado);
+
+        btnMostrarProveedores.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new VentanaListaProveedores(proveedores);
+            }
+        });
+
+        btnAgregar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            }
+        });
+
+        btnVolver.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        panelCentral.add(panelID);
+        panelCentral.add(panelNombre);
+        panelCentral.add(panelPrecio);
+        panelCentral.add(panelGama);
+        panelCentral.add(panelProveedorID);
+
+        add(panelSuperior, BorderLayout.NORTH);
+        add(panelCentral, BorderLayout.CENTER);
+        add(panelInferior, BorderLayout.SOUTH);
+
+        setVisible(true);
+
+        addWindowListener(new WindowAdapter() {
+        });
     }
 }
